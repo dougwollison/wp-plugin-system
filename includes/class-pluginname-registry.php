cb756be3ea0b01f9fbb79f624bf5c0b27a187ac4
Doug Wollison <Doug@Dougs-iMac.local>
2016-03-29 10:08:11 -0400

    Add override feature from nLingual

--- includes/class-pluginname-registry.php
+++ includes/class-pluginname-registry.php
@@ -19,8 +19,7 @@ namespace PluginName;
  *
  * @since 1.0.0
  */
-
-class Registry {
+final class Registry {
 	// =========================
 	// ! Properties
 	// =========================
@@ -69,6 +68,17 @@ class Registry {
 	 */
 	protected static $options_deprecated = array();
 
+	/**
+	 * The current-state option overrides.
+	 *
+	 * @internal
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var array
+	 */
+	private static $option_overrides = array();
+
 	// =========================
 	// ! Property Accessing
 	// =========================
@@ -82,7 +92,7 @@ class Registry {
 	 *
 	 * @return array The options whitelist.
 	 */
-	public static function get_defaults() {
+	final public static function get_defaults() {
 		return static::$options_whitelist;
 	}
 
@@ -98,7 +108,7 @@ class Registry {
 	 *
 	 * @return bool Wether or not the option is supported.
 	 */
-	public static function has( &$option ) {
+	final public static function has( &$option ) {
 		if ( isset( static::$options_deprecated[ $option ] ) ) {
 			$option = static::$options_deprecated[ $option ];
 		}
@@ -111,20 +121,28 @@ class Registry {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param string $option  The option name.
-	 * @param mixed  $default Optional. The default value to return.
+	 * @param string $option       The option name.
+	 * @param mixed  $default      Optional. The default value to return.
+	 * @param bool   $true_value   Optional. Get the true value, bypassing any overrides.
+	 * @param bool   $has_override Optional. By-reference boolean to identify if an override exists.
 	 *
 	 * @return mixed The property value.
 	 */
-	public static function get( $option, $default = null ) {
+	final public static function get( $option, $default = null, $true_value = false, &$has_override = null ) {
 		// Trigger notice error if trying to set an unsupported option
 		if ( ! static::has( $option ) ) {
-			trigger_error( "[PluginName] The option '{$option}' is not supported", E_USER_NOTICE );
+			trigger_error( "[PluginName] The option '{$option}' is not supported.", E_USER_NOTICE );
 		}
 
 		// Check if it's set, return it's value.
 		if ( isset( static::$options[ $option ] ) ) {
-			$value = static::$options[ $option ];
+			// Check if it's been overriden, use that unless otherwise requested
+			$has_override = isset( static::$option_overrides[ $option ] );
+			if ( $has_override && ! $true_value ) {
+				$value = static::$option_overrides[ $option ];
+			} else {
+				$value = static::$options[ $option ];
+			}
 		} else {
 			$value = $default;
 		}
@@ -142,13 +160,32 @@ class Registry {
 	 * @param string $option The option name.
 	 * @param mixed  $value  The value to assign.
 	 */
-	public static function set( $option, $value = null ) {
+	final public static function set( $option, $value = null ) {
 		// Trigger notice error if trying to set an unsupported option
 		if ( ! static::has( $option ) ) {
 			trigger_error( "[PluginName] The option '{$option}' is not supported", E_USER_NOTICE );
 		}
 
 		static::$options[ $option ] = $value;
+	}
+
+	/**
+	 * Temporarily override an option value.
+	 *
+	 * These options will be retrieved when using get(), but will not be saved.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $option The option name.
+	 * @param mixed  $value  The value to override with.
+	 */
+	final public static function override( $option, $value ) {
+		// Trigger notice error if trying to set an unsupported option
+		if ( ! static::has( $option ) ) {
+			trigger_error( "[PluginName] The option '{$option}' is not supported.", E_USER_NOTICE );
+		}
+
+		static::$options_override[ $option ] = $value;
 	}
 
 	// =========================
@@ -166,7 +203,7 @@ class Registry {
 	 *
 	 * @param bool $reload Should we reload the options?
 	 */
-	public static function load( $reload = false ) {
+	final public static function load( $reload = false ) {
 		if ( static::$__loaded && ! $reload ) {
 			// Already did this
 			return;
@@ -197,7 +234,7 @@ class Registry {
 	 *
 	 * @param string $what Optional. Save just options/languages or both (true)?
 	 */
-	public static function save( $what = true ) {
+	final public static function save( $what = true ) {
 		update_option( 'pluginname_options', static::$options );
 	}
 }
