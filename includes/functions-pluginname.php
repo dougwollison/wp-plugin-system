27b8864663273f3e152fd1f58a50b8795631028f
Doug Wollison <doug@wollison.net>
2017-04-26 14:40:29 -0400

    Update is_backend logic

--- includes/functions-pluginname.php
+++ includes/functions-pluginname.php
@@ -26,15 +26,18 @@ namespace PluginName;
  * @global string $pagenow The current page slug.
  */
 function is_backend() {
-	global $pagenow;
+	if ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) {
+		// "Install" process, count as backend
+		return true;
+	}
 
 	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 		// AJAX request, check if the referrer is from wp-admin
 		return strpos( $_SERVER['HTTP_REFERER'], admin_url() ) === 0;
-	} else {
-		// Check if in the admin or otherwise the login/register page
-		return is_admin() || in_array( $pagenow, array( 'wp-login.php', 'wp-register.php' ) );
 	}
+
+	// Check if in the admin or otherwise the login/register page
+	return is_admin() || in_array( $_SERVER['SCRIPT_NAME'], array( 'wp-login.php', 'wp-register.php' ) );
 }
 
 // =========================
